bf6346e76ef2e649a40f95816eca18c33b9f1f24
molamolaxxx <408287749@qq.com>
2023-01-23 16:04:35 +0800

    单点性能测试与环路测试

---
+++ rpc-test/rpc-consumer/src/test/java/com/mola/rpc/consumer/test/LoopBackTest.java
@@ -0,0 +1,8 @@
+package com.mola.rpc.consumer.test;/**
+* @Project: my-rpc
+* @Description: 
+* @author : molamola
+* @date : 2023-01-23 15:21
+**/
+public class LoopBackTest {
+}
---
+++ rpc-test/rpc-consumer/src/test/java/com/mola/rpc/consumer/test/UserServiceTestImpl.java
@@ -0,0 +1,8 @@
+package com.mola.rpc.consumer.test;/**
+* @Project: my-rpc
+* @Description: 
+* @author : molamola
+* @date : 2023-01-23 15:42
+**/
+public class UserServiceTestImpl {
+}
